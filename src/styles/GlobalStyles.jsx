e73650fb9608a32455b7eb6137c99390ef4797a0
Annie <anniehartmann97@gmail.com>
2022-07-14 23:30:40 +0300

    More settings

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    font-family: 'Inter', sans-serif;
+  }
+  body {
+    font-size: 16px;
+    background: #F9F9F9;
+  }
+  li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  a {
+    font-family: 'Inter', sans-serif;
+    text-decoration: none;
+  }
+  button {
+    border: none;
+    outline: none;
+    background: none;
+  }
+`;
+export default GlobalStyle;
